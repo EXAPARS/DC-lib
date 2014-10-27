b50b86f8b4c993a141df56a68d1ec5b2df59d70f
lthebault <loic.thebault@prism.uvsq.fr>
2014-10-27 18:51:58 +0100

    Add LGPL header to all src files

--- src/headers/partitioning.h
+++ src/headers/partitioning.h
@@ -1,3 +1,19 @@
+/*  Copyright 2014 - UVSQ
+    Authors list: Loïc Thébault, Eric Petit
+
+    This file is part of the D&C library.
+
+    D&C library is free software: you can redistribute it and/or modify it under the
+    terms of the GNU Lesser General Public License as published by the Free Software
+    Foundation, either version 3 of the License, or (at your option) any later version.
+
+    D&C library is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+    PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with
+    the D&C library. If not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef PARTITIONING_H
 #define PARTITIONING_H
 
